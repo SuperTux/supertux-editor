7f2cfbc2bdda2826539517891d74309df0ce02a7
Matthias Braun <matze@braunis.de>
2006-04-02 15:07:20 +0000

    support resizing of sectors, support cloning of objects, draw frame around sector
    SVN-Revision: 3189

---
+++ libeditor/SceneGraph/Rect.cs
@@ -0,0 +1,34 @@
+using System;
+using DataStructures;
+using OpenGl;
+
+namespace SceneGraph
+{
+	
+	public class Rectangle : Node
+	{
+		public RectangleF Rect;
+		public bool Fill;
+		
+		public Rectangle()
+		{
+		}
+		
+		public void Draw()
+		{
+			gl.Disable(gl.TEXTURE_2D);
+			gl.PolygonMode(gl.FRONT_AND_BACK, Fill ? gl.FILL : gl.LINE);
+			
+			gl.Begin(gl.QUADS);
+			gl.Vertex2f(Rect.Left, Rect.Top);
+			gl.Vertex2f(Rect.Right, Rect.Top);
+			gl.Vertex2f(Rect.Right, Rect.Bottom);
+			gl.Vertex2f(Rect.Left, Rect.Bottom);
+			gl.End();
+			
+			gl.PolygonMode(gl.FRONT_AND_BACK, gl.FILL);			
+			gl.Enable(gl.TEXTURE_2D);
+		}
+	}
+	
+}
