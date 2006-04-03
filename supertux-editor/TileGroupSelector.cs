5b4c345647e7cc3378dc94e3a4dcf64fe26d3667
Matthias Braun <matze@braunis.de>
2006-04-03 11:47:24 +0000

    forgot another file
    SVN-Revision: 3213

---
+++ supertux-editor/TileGroupSelector.cs
@@ -0,0 +1,59 @@
+using System;
+using Gtk;
+
+public class TilegroupSelector : ComboBox
+{
+	private Level level;
+	private TileListWidget tileList;
+	
+	public TilegroupSelector(IEditorApplication application, TileListWidget tileList)
+	{
+		this.tileList = tileList;
+		application.LevelChanged += OnLevelChanged;
+		
+		CellRendererText renderer = new CellRendererText(); 
+		PackStart(renderer, false);
+		SetCellDataFunc(renderer, TextDataFunc);		            
+		
+		Changed += OnTileGroupChoosen;
+	}
+	
+	private void OnLevelChanged(Level level)
+	{
+		if(this.level != null)
+			this.level.TilesetChanged -= OnTilesetChanged;
+		if(level != null)
+			level.TilesetChanged += OnTilesetChanged;
+		this.level = level;
+		
+		OnTilesetChanged(level);
+	}
+	
+	private void OnTilesetChanged(Level level)
+	{
+		Tileset tileset = level.Tileset;
+		
+		TreeStore store = new TreeStore(typeof(Tilegroup));
+		//store.AppendValues(null);
+		foreach(Tilegroup group in tileset.Tilegroups.Values) {
+			store.AppendValues(group);
+		}
+		Model = store;
+	}
+	
+	private void TextDataFunc(CellLayout cell_layout, CellRenderer renderer, TreeModel model, TreeIter iter)
+	{
+		CellRendererText textRenderer = (CellRendererText) renderer;
+		Tilegroup group = (Tilegroup) Model.GetValue(iter, 0);
+		
+		if(group == null)
+			textRenderer.Text = "All";
+		else
+			textRenderer.Text = group.Name;
+	}	
+	
+	private void OnTileGroupChoosen(object o, EventArgs args)
+	{
+		//Console.WriteLine(	
+	}
+}
