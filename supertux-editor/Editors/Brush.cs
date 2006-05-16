5455259a1973d2be5235cd0161d3fabbfc16c25f
Christoph Sommer <mail@christoph-sommer.de>
2006-05-16 13:28:20 +0000

    More work on Brush and BrushEditor. Loading and painting works. Interface still missing.
    SVN-Revision: 3536

--- supertux-editor/Editors/Brush.cs
+++ supertux-editor/Editors/Brush.cs
@@ -1,7 +1,6 @@
-//  $Id: foo.cpp 2979 2006-01-10 00:00:04Z sommer $
+//  $Id$
 // 
-//  Cobble - A simple SuperTux level editor
-//  Copyright (C) 2006 Christoph Sommer <supertux@2006.expires.deltadevelopment.de>
+//  Copyright (C) 2006 Christoph Sommer <christoph.sommer@2006.expires.deltadevelopment.de>
 //
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
@@ -17,165 +16,193 @@
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 //  02111-1307, USA.
+
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using DataStructures;
 
+/// <summary>
+/// Smoothes Tilemaps by changing tiles to one of several stored valid patterns
+/// </summary>
 class Brush 
 {
-    protected List<TileBlock> patterns;
+	protected List<Field<int>> patterns;
 	protected uint width;
-    protected uint height;
-    protected Tileset tileset;
+	protected uint height;
+	protected Tileset tileset;
 
-	public Brush(uint width, uint height, List<TileBlock> patterns, Tileset tileset)
+	public Brush(uint width, uint height, List<Field<int>> patterns, Tileset tileset)
 	{
-        this.width = width;
-        this.height = height;
-        this.patterns = patterns;
-        this.tileset = tileset;
+		this.width = width;
+		this.height = height;
+		this.patterns = patterns;
+		this.tileset = tileset;
 	}
 
 	public Brush(uint width, uint height, Tileset tileset)
 	{
-        this.width = width;
-        this.height = height;
-        this.patterns = new List<TileBlock>();
-        this.tileset = tileset;
-    }
-	
+		this.width = width;
+		this.height = height;
+		this.patterns = new List<Field<int>>();
+		this.tileset = tileset;
+	}
+
 	public int PatternCount {
 		get {
 			return patterns.Count;
 		}
 	}
 
-    public uint Width {
-        get {
-            return width;
-        }
-    }
+	public uint Width {
+		get {
+			return width;
+		}
+	}
 
-    public uint Height {
-        get {
-            return height;
-        }
-    }
+	public uint Height {
+		get {
+			return height;
+		}
+	}
 
-    /// <summary>
-    /// Add the tiles of the given tileBlock as a valid pattern
-    /// </summary>
-    /// <param name="tileBlock">tileBlock that specifies the pattern to add</param>
-    public void LearnPattern(TileBlock tileBlock, int startX, int startY) {
-        TileBlock tb = (TileBlock)tileBlock.CloneSubset(startX, startY, width, height);
-        if (!patterns.Contains(tb)) patterns.Add(tb);
-    }
+	/// <summary>
+	/// Add the tiles of the given tileBlock as a valid pattern
+	/// </summary>
+	/// <param name="tileBlock">tileBlock that specifies the pattern to add</param>
+	// FIXME: untested
+	public void LearnPattern(Field<int> tileBlock, int startX, int startY) {
+		Field<int> tb = (Field<int>)tileBlock.CloneSubset(startX, startY, width, height);
+		if (!patterns.Contains(tb)) patterns.Add(tb);
+	}
 
-    /// <summary>
-    /// Add all tiles of the given tileBlock to the list of valid patterns
-    /// </summary>
-    /// <param name="tileBlock">tileBlock that specifies the patterns to add</param>
-    public void LearnPatterns(TileBlock tileBlock) {
-        for (int tx = 0; tx < tileBlock.Width - width; tx++) {
-            for (int ty = 0; ty < tileBlock.Height - height; ty++) {
-                LearnPattern(tileBlock, tx, ty);
-            }
-        }
-    }
+	/// <summary>
+	/// Add all tiles of the given tileBlock to the list of valid patterns
+	/// </summary>
+	/// <param name="tileBlock">tileBlock that specifies the patterns to add</param>
+	// FIXME: untested
+	public void LearnPatterns(Field<int> tileBlock) {
+		for (int tx = 0; tx < tileBlock.Width - width; tx++) {
+			for (int ty = 0; ty < tileBlock.Height - height; ty++) {
+				LearnPattern(tileBlock, tx, ty);
+			}
+		}
+	}
 
-    /// <summary>
-    /// Remove the tiles of the given tileBlock from the list of valid patterns
-    /// </summary>
-    /// <param name="tileBlock">tileBlock that specifies the pattern to remove</param>
-    public void ForgetPattern(TileBlock tileBlock, int startX, int startY) {
-        TileBlock tb = (TileBlock)tileBlock.CloneSubset(startX, startY, width, height);
-        if (patterns.Contains(tb)) patterns.Remove(tb);
-    }
+	/// <summary>
+	/// Remove the tiles of the given tileBlock from the list of valid patterns
+	/// </summary>
+	/// <param name="tileBlock">tileBlock that specifies the pattern to remove</param>
+	// FIXME: untested
+	public void ForgetPattern(Field<int> tileBlock, int startX, int startY) {
+		Field<int> tb = (Field<int>)tileBlock.CloneSubset(startX, startY, width, height);
+		if (patterns.Contains(tb)) patterns.Remove(tb);
+	}
 
-    protected float calculateSimilarity(TileBlock t1, TileBlock t2) {
-        float sim = 0;
-        if (t1.Width != t2.Width) throw new ArgumentException("TileBlocks had different widths");
-        if (t1.Height != t2.Height) throw new ArgumentException("TileBlocks had different heights");
-        for (int px = 0; px < t1.Width; px++) {
-            for (int py = 0; py < t1.Height; py++) {
-                int id1 = t1[px, py];
-                int id2 = t2[px, py];
-                bool solid1 = ((tileset.Get(id1).Attributes & Tile.Attribute.SOLID) != 0);
-                bool solid2 = ((tileset.Get(id2).Attributes & Tile.Attribute.SOLID) != 0);
-                if (id1 == id2) sim += 1;
-                if (solid1 == solid2) sim += 100;
-            }
-        }
-        return sim;
-    }
+	protected float calculateSimilarity(Field<int> t1, Field<int> t2) {
+		float sim = 0;
+		if (t1.Width != t2.Width) throw new ArgumentException("Field<int>s had different widths");
+		if (t1.Height != t2.Height) throw new ArgumentException("Field<int>s had different heights");
+		for (int px = 0; px < t1.Width; px++) {
+			for (int py = 0; py < t1.Height; py++) {
+				int id1 = t1[px, py];
+				int id2 = t2[px, py];
+				Tile tile1 = tileset.Get(id1);
+				Tile tile2 = tileset.Get(id2);
+				if (tile1 == null) return -1;
+				if (tile2 == null) return -1;
+				bool solid1 = ((tile1.Attributes & Tile.Attribute.SOLID) != 0);
+				bool solid2 = ((tile2.Attributes & Tile.Attribute.SOLID) != 0);
+				if (id1 == id2) sim += 1;
+				if (solid1 == solid2) sim += 100;
+			}
+		}
+		return sim;
+	}
 
-	public void ApplyToTilemap(FieldPos pos, Tilemap tilemap, Tileset tileset) {
-        int px = pos.X - (int)(width/2);
-        int py = pos.Y - (int)(height/2);
+	/// <summary>
+	/// Smoothes Tilemap by changing tiles around the given position to one of several stored valid patterns
+	/// </summary>
+	public void ApplyToTilemap(FieldPos pos, Tilemap tilemap) {
+		int px = pos.X - (int)(width/2);
+		int py = pos.Y - (int)(height/2);
 		if (px < 0) return;
 		if (py < 0) return;
 		if (px+width > tilemap.Width) return;
 		if (py+width > tilemap.Height) return;
 
-        TileBlock tb = (TileBlock)tilemap.CloneSubset(px, py, width, height);
+		Field<int> tb = (Field<int>)tilemap.CloneSubset(px, py, width, height);
 
-        float bestSimilarity = 0;
-        TileBlock bestPattern = null;
-        foreach (TileBlock pattern in patterns) {
-            float sim = calculateSimilarity(pattern, tb);
-            if (sim > bestSimilarity) {
-                bestSimilarity = sim;
-                bestPattern = pattern;
-            }
-        }
-        if (bestPattern != null) {
-            bestPattern.ApplyToTilemap(new FieldPos(px, py), tilemap);
-        }
+		float bestSimilarity = 0;
+		Field<int> bestPattern = null;
+		foreach (Field<int> pattern in patterns) {
+			float sim = calculateSimilarity(pattern, tb);
+			if (sim > bestSimilarity) {
+				bestSimilarity = sim;
+				bestPattern = pattern;
+			}
+		}
+		if (bestPattern != null) {
+			uint StartX = (uint) Math.Max(0, -px);
+			uint StartY = (uint) Math.Max(0, -py);
+			uint W = Math.Min((uint) (tilemap.Width - px), Width);
+			uint H = Math.Min((uint) (tilemap.Height - py), Height);
+			for(uint y = StartY; y < H; ++y) {
+				for(uint x = StartX; x < W; ++x) {
+					tilemap[(uint) (px + x), (uint) (py + y)] = bestPattern[x, y];
+				}
+			}
+		}
 	}
-	
+
+	// FIXME: untested
+	// TODO: change file syntax from CSV to Lisp
 	public void saveToFile(string fname) {
 		FileStream fs = new FileStream(fname, FileMode.Create);
 		TextWriter tw = new StreamWriter(fs);
 
-		foreach (TileBlock m1 in patterns) {
-			tw.WriteLine("" + m1[0, 0] + "," + m1[0, 1] + "," + m1[0, 2] + "," + m1[1, 0] + "," + m1[1, 1] + "," + m1[1, 2] + "," + m1[2, 0] + "," + m1[2, 1] + "," + m1[2, 2] + "");
+		foreach (Field<int> m1 in patterns) {
+			tw.WriteLine("" + m1[0, 0] + "," + m1[1, 0] + "," + m1[2, 0] + "," + m1[0, 1] + "," + m1[1, 1] + "," + m1[2, 1] + "," + m1[0, 2] + "," + m1[1, 2] + "," + m1[2, 2] + "");
 		}
 
 		tw.Close();
 		fs.Close();
 	}
 
+	/// <summary>
+	/// Creates a new Brush by loading a list of valid patterns from the given file
+	/// </summary>
+	// TODO: change file syntax from CSV to Lisp
 	public static Brush loadFromFile(string fname, Tileset tileset) {
 		FileStream fs = new FileStream(fname, FileMode.Open);
 		TextReader trd = new StreamReader(fs);
 
-        Brush brush = new Brush(3, 3, tileset);
+		Brush brush = new Brush(3, 3, tileset);
 
 		try {
 			string s;
 			while ((s = trd.ReadLine()) != null) {
 				string[] v = s.Split(',');
 				if (v.Length < 9) continue;
-				TileBlock tb = (TileBlock)new Field<int>(3,3,0);
-                tb[0, 0] = int.Parse(v[0]);
-                tb[0, 1] = int.Parse(v[1]);
-                tb[0, 2] = int.Parse(v[2]);
-                tb[1, 0] = int.Parse(v[3]);
-                tb[1, 1] = int.Parse(v[4]);
-                tb[1, 2] = int.Parse(v[5]);
-                tb[1, 0] = int.Parse(v[6]);
-                tb[1, 1] = int.Parse(v[7]);
-                tb[1, 2] = int.Parse(v[8]);
-                if (!brush.patterns.Contains(tb)) brush.patterns.Add(tb);
+				Field<int> tb = (Field<int>)new Field<int>(3,3,0);
+				tb[0, 0] = int.Parse(v[0]);
+				tb[1, 0] = int.Parse(v[1]);
+				tb[2, 0] = int.Parse(v[2]);
+				tb[0, 1] = int.Parse(v[3]);
+				tb[1, 1] = int.Parse(v[4]);
+				tb[2, 1] = int.Parse(v[5]);
+				tb[0, 2] = int.Parse(v[6]);
+				tb[1, 2] = int.Parse(v[7]);
+				tb[2, 2] = int.Parse(v[8]);
+				if (!brush.patterns.Contains(tb)) brush.patterns.Add(tb);
 			}
 		} finally {
 			trd.Close();
 			fs.Close();
 		}
 
-        return brush;
-   }	
+		return brush;
+	}	
 
 }
---
+++ supertux-editor/Editors/BrushEditor.cs
@@ -0,0 +1,199 @@
+using DataStructures;
+using OpenGl;
+using System;
+using Gdk;
+
+/// <summary>
+/// Smoothes Tilemaps by changing tiles to one of several stored valid patterns
+/// </summary>
+// TODO: create interface for loading and saving of brushes
+// TODO: implement learning and forgetting patterns
+public class BrushEditor : IEditor, IDisposable {
+	private Selection selection;
+	private bool drawing;
+	private bool selecting;	
+	private FieldPos MouseTilePos;
+	private FieldPos SelectStartPos;
+	private FieldPos LastDrawPos;
+	private FieldPos SelectionP1;
+	private FieldPos SelectionP2;
+
+	private Tilemap Tilemap;
+	private Tileset Tileset;
+	private Brush brush;
+
+	public event RedrawEventHandler Redraw;
+
+	public BrushEditor(Tilemap Tilemap, Tileset Tileset, Selection selection)
+	{
+		this.Tilemap = Tilemap;
+		this.Tileset = Tileset;
+		this.selection = selection;
+		selection.Changed += OnSelectionChanged;
+		brush = Brush.loadFromFile("/tmp/ice.csv", Tileset);
+	}
+
+	public void Dispose()
+	{
+		selection.Changed -= OnSelectionChanged;
+	}
+
+	public void Draw()
+	{
+		if(!selecting) {
+			// draw white rectangle over affected tiles
+			float px = (MouseTilePos.X - (int)(brush.Width / 2)) * 32f;
+			float py = (MouseTilePos.Y - (int)(brush.Height / 2)) * 32f;
+			float w = brush.Width * 32f;
+			float h = brush.Height * 32f;
+
+			gl.Color4f(1, 1, 1, 0.25f);
+			gl.Disable(gl.TEXTURE_2D);
+			gl.Begin(gl.QUADS);
+			gl.Vertex2f(px, py);
+			gl.Vertex2f(px+w, py);
+			gl.Vertex2f(px+w, py+h);
+			gl.Vertex2f(px, py+h);
+			gl.End();
+			gl.Enable(gl.TEXTURE_2D);
+			gl.Color4f(1, 1, 1, 1);
+		}
+		if(selecting) {
+			gl.Color4f(0, 0, 1, 0.7f);
+			gl.Disable(gl.TEXTURE_2D);
+
+			float left = SelectionP1.X * 32f;
+			float top = SelectionP1.Y * 32f;
+			float right = SelectionP2.X * 32f + 32f;
+			float bottom = SelectionP2.Y * 32f + 32f;
+
+			gl.Begin(gl.QUADS);
+			gl.Vertex2f(left, top);
+			gl.Vertex2f(right, top);
+			gl.Vertex2f(right, bottom);
+			gl.Vertex2f(left, bottom);
+			gl.End();
+
+			gl.Enable(gl.TEXTURE_2D);
+			gl.Color4f(1, 1, 1, 1);
+		}
+	}
+
+	public void OnMouseButtonPress(Vector MousePos, int button, ModifierType Modifiers)
+	{
+		UpdateMouseTilePos(MousePos);
+	
+		if(button == 1) {
+			brush.ApplyToTilemap(MouseTilePos, Tilemap);
+			LastDrawPos = MouseTilePos;
+			drawing = true;
+			Redraw();
+		}
+		if(button == 3) {
+			if(MouseTilePos.X < 0 || MouseTilePos.Y < 0
+				|| MouseTilePos.X >= Tilemap.Width
+				|| MouseTilePos.Y >= Tilemap.Height)
+				return;
+			
+			SelectStartPos = MouseTilePos;
+			selecting = true;
+			UpdateSelection();	
+			Redraw();
+		}
+	}
+
+	public void OnMouseButtonRelease(Vector MousePos, int button, ModifierType Modifiers)
+	{
+		UpdateMouseTilePos(MousePos);
+	
+		if(button == 1) {
+			drawing = false;
+		}
+		if(button == 3) {
+			UpdateSelection();
+
+			uint NewWidth = (uint) (SelectionP2.X - SelectionP1.X) + 1;
+			uint NewHeight = (uint) (SelectionP2.Y - SelectionP1.Y) + 1;
+			selection.Resize(NewWidth, NewHeight, 0);
+			for(uint y = 0; y < NewHeight; y++) {
+				for(uint x = 0; x < NewWidth; ++x) {
+					selection[x, y] 
+						= Tilemap[(uint) SelectionP1.X + x,
+						  		  (uint) SelectionP1.Y + y];
+				}
+			}
+
+			selection.FireChangedEvent();
+			selecting = false;
+		}
+
+		Redraw();
+	}
+
+	public void OnMouseMotion(Vector MousePos, ModifierType Modifiers)
+	{
+		if(UpdateMouseTilePos(MousePos)) {
+			if(selection.Width == 0 || selection.Height == 0)
+				return;
+
+			if(drawing &&
+					( (Modifiers & ModifierType.ShiftMask) != 0 ||
+				((LastDrawPos.X - MouseTilePos.X) % selection.Width == 0 &&
+				 (LastDrawPos.Y - MouseTilePos.Y) % selection.Height == 0))) {
+				LastDrawPos = MouseTilePos;
+				brush.ApplyToTilemap(MouseTilePos, Tilemap);
+			}
+			if(selecting)
+				UpdateSelection();
+			Redraw();
+		}
+	}
+
+	private bool UpdateMouseTilePos(Vector MousePos)
+	{
+		FieldPos NewMouseTilePos = new FieldPos(
+				(int) (MousePos.X) / 32,
+				(int) (MousePos.Y) / 32);
+		if(NewMouseTilePos != MouseTilePos) {
+			MouseTilePos = NewMouseTilePos;
+			return true;
+		}
+
+		return false;
+	}
+
+	private void UpdateSelection()
+	{
+		if(MouseTilePos.X < SelectStartPos.X) {
+			if(MouseTilePos.X < 0)
+				SelectionP1.X = 0;
+			else
+				SelectionP1.X = MouseTilePos.X;
+			SelectionP2.X = SelectStartPos.X;
+		} else {
+			SelectionP1.X = SelectStartPos.X;
+			if(MouseTilePos.X >= Tilemap.Width)
+				SelectionP2.X = (int) Tilemap.Width - 1;
+			else
+				SelectionP2.X = MouseTilePos.X;
+		}
+
+		if(MouseTilePos.Y < SelectStartPos.Y) {
+			if(MouseTilePos.Y < 0)
+				SelectionP1.Y = 0;
+			else
+				SelectionP1.Y = MouseTilePos.Y;
+			SelectionP2.Y = SelectStartPos.Y;
+		} else {
+			SelectionP1.Y = SelectStartPos.Y;
+			if(MouseTilePos.Y >= Tilemap.Height)
+				SelectionP2.Y = (int) Tilemap.Height - 1;
+			else
+				SelectionP2.Y = MouseTilePos.Y;
+		}
+	}
+	
+	private void OnSelectionChanged() {
+		Redraw();
+	}
+}
