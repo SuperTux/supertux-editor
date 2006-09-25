d39fd774cec1e625579db43d7adbc39a87beacf9
Arvid Norlander <anmaster@berlios.de>
2006-09-25 20:20:38 +0000

    Editor (0.3.x branch): Fixed title for editor, and changed version constant.
    SVN-Revision: 4267

--- supertux-editor/Constants.cs
+++ supertux-editor/Constants.cs
@@ -1,4 +1,4 @@
 public static class Constants {
 	public const string PACKAGE_NAME = "supertux-editor";
-	public const string PACKAGE_VERSION = "0.2";
+	public const string PACKAGE_VERSION = "0.3-svn";
 }
