b4eb302d1f065b986ea697fcfa36efb934f663e6
Arvid Norlander <anmaster@berlios.de>
2007-01-12 08:51:23 +0000

    Editor: Removed trailing whitespaces
    SVN-Revision: 4546

--- supertux-editor/Application.cs
+++ supertux-editor/Application.cs
@@ -515,7 +515,7 @@ public class Application : IEditorApplication {
 		dialog.Comments = "A level and worldmap editor for SuperTux 0.3.0";
 		dialog.Authors = authors;
 		dialog.Copyright = "Copyright (c) 2006 SuperTux Devel Team";
-		dialog.License = 
+		dialog.License =
 			"This program is free software; you can redistribute it and/or modify\n" +
 			"it under the terms of the GNU General Public License as published by\n" +
 			"the Free Software Foundation; either version 2 of the License, or\n" +
