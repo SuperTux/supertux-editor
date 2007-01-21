c128cb2246fc9f113b5a1654e924fa58456b05d4
Matthias Braun <matze@braunis.de>
2007-01-21 14:42:19 +0000

    some more docu
    SVN-Revision: 4621

--- libeditor/SceneGraph/Transform.cs
+++ libeditor/SceneGraph/Transform.cs
@@ -6,6 +6,11 @@ using OpenGl;
 namespace SceneGraph
 {
 
+	/// <summary>
+	/// A scene graph node which applies a transform to the current opengl
+	/// transform (the matrix will be multiplied with the currently active
+	/// tranform)
+	/// </summary>
 	public sealed class Transform : NodeWithChilds
 	{
 		public Matrix Matrix;
