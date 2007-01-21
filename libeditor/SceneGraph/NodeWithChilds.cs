b02b8e34d880024850ccfee6da8d2d08d5e1aefb
Matthias Braun <matze@braunis.de>
2007-01-21 14:43:15 +0000

    some more docu
    SVN-Revision: 4622

--- libeditor/SceneGraph/NodeWithChilds.cs
+++ libeditor/SceneGraph/NodeWithChilds.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 namespace SceneGraph
 {
 
+	/// <summar>
+	/// A base class that allows constructing scene graph nodes that have
+	/// several childs
+	/// </summar>
 	public class NodeWithChilds : Node
 	{
 		private List<Node> Childs = new List<Node>();
