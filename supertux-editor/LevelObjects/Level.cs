45950c910d0fe860c51c897b4dae2757d0baba5b
Christoph Sommer <mail@christoph-sommer.de>
2007-03-07 21:56:35 +0000

    Added "Contact" and "License" level properties
    SVN-Revision: 4929

--- supertux-editor/LevelObjects/Level.cs
+++ supertux-editor/LevelObjects/Level.cs
@@ -15,9 +15,15 @@ public sealed class Level
 	[PropertyProperties(Tooltip = "Name of this level.")]
 	[LispChild("name", Translatable = true)]
 	public string Name = String.Empty;
-	[PropertyProperties(Tooltip = "Author of this level.")]
+	[PropertyProperties(Tooltip = "Author of this level, e.g. \"John Doe\"")]
 	[LispChild("author")]
 	public string Author = String.Empty;
+	[PropertyProperties(Tooltip = "How to contact the author, e.g. \"mailto:john.doe@example.com\"")]
+	[LispChild("contact", Optional = true, Default = "")]
+	public string Contact = "";
+	[PropertyProperties(Tooltip = "Whether to allow the community to share the level, improve it, include it in bonus packs, ...")]
+	[LispChild("license", Optional = true, Default = ""), ChooseLicenseSetting()]
+	public string License = "";
 
 	private string tilesetFile = "images/tiles.strf";
 	public Tileset Tileset = new Tileset("images/tiles.strf");
---
+++ supertux-editor/PropertyEditors/ChooseLicenseWidget.cs
@@ -0,0 +1,69 @@
+//  $Id$
+//
+//  Copyright (C) 2007 Christoph Sommer <christoph.sommer@2007.expires.deltadevelopment.de>
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+//  02111-1307, USA.
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Gtk;
+using LispReader;
+
+public sealed class ChooseLicenseWidget : CustomSettingsWidget {
+	private ComboBoxEntry comboBox;
+
+	public override Widget Create(object caller) {
+
+		List<String> licenseTemplateTexts = new List<String>();
+		licenseTemplateTexts.Add("non-redistributable (forbid sharing and modification of this level)");
+		licenseTemplateTexts.Add("GPL 2 / CC-by-sa 3.0 (allow sharing and modification of this level)");
+
+		comboBox = new ComboBoxEntry(licenseTemplateTexts.ToArray());
+		comboBox.Changed += OnComboBoxChanged;
+
+		HBox box = new HBox();
+		box.PackStart(comboBox, true, true, 0);
+		box.Name = field.Name;
+
+		CreateToolTip(caller, comboBox);
+
+		return box;
+	}
+
+	private void OnComboBoxChanged(object o, EventArgs args) {
+		try {
+			ComboBoxEntry comboBox = (ComboBoxEntry)o;
+			string s = comboBox.ActiveText;
+
+			// strip off comments from licenseTemplateTexts
+			if (s == "non-redistributable (forbid sharing and modification of this level)") s = s.Substring(0, s.IndexOf(" ("));
+			if (s == "GPL 2 / CC-by-sa 3.0 (allow sharing and modification of this level)") s = s.Substring(0, s.IndexOf(" ("));
+
+			comboBox.Entry.Text = s;
+			field.SetValue(_object, s);
+		} catch (Exception e) {
+			ErrorDialog.Exception(e);
+		}
+	}
+}
+
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)] 
+public sealed class ChooseLicenseSettingAttribute : CustomSettingsWidgetAttribute {
+	public ChooseLicenseSettingAttribute() : base(typeof(ChooseLicenseWidget)) {
+	}
+}
+
