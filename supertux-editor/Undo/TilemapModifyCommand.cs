5f5bf138bdb3c89be8d06e3b18f80abd41513aa1
Miloš Klouček <m.kloucek.m@atlas.cz>
2008-06-07 18:52:55 +0000

    Fixed copyright text & code author
    SVN-Revision: 5535

--- supertux-editor/Undo/TilemapModifyCommand.cs
+++ supertux-editor/Undo/TilemapModifyCommand.cs
@@ -1,5 +1,6 @@
+//  $Id$
 //
-//  Copyright (C) 2007 Arvid Norlander <anmaster AT berlios DOT de>
+//  Copyright (C) 2007 SuperTux Devel Team
 //
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
@@ -15,6 +16,7 @@
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 //  02111-1307, USA.
+
 using System;
 using LispReader;
 
