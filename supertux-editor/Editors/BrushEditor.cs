389d3395d47781b90d24bbc20770545c370d6e98
Miloš Klouček <m.kloucek.m@atlas.cz>
2008-06-07 13:35:04 +0000

    Fixed command order in drawing method
    SVN-Revision: 5529

--- supertux-editor/Editors/BrushEditor.cs
+++ supertux-editor/Editors/BrushEditor.cs
@@ -129,25 +129,7 @@ public sealed class BrushEditor : TileEditorBase, IEditor {
 
 		// When selecting, draw blue rectangle over selected area
 		if(selecting) {
-
-			// Calculate rectangle to color
-			float left = SelectionP1.X * 32f;
-			float top = SelectionP1.Y * 32f;
-			float right = SelectionP2.X * 32f + 32f;
-			float bottom = SelectionP2.Y * 32f + 32f;
-
-			// Draw rectangle
-			gl.Color4f(0, 0, 1, 0.7f);
-			gl.Disable(gl.TEXTURE_2D);
-			gl.Begin(gl.QUADS);
-			gl.Vertex2f(left, top);
-			gl.Vertex2f(right, top);
-			gl.Vertex2f(right, bottom);
-			gl.Vertex2f(left, bottom);
-			gl.End();
-			gl.Enable(gl.TEXTURE_2D);
-			gl.Color4f(1, 1, 1, 1);
-
+			base.Draw(cliprect);
 		}
 	}
 
