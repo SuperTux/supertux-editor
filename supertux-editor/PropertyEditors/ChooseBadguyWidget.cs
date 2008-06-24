ef31c5a8ac716fde28b2f9c6323d6b0ae868a627
Miloš Klouček <m.kloucek.m@atlas.cz>
2008-06-24 14:42:36 +0000

    EDITOR: Removed old+temporary Dispenser.badguy one-line editor.
    SVN-Revision: 5622

--- supertux-editor/PropertyEditors/ChooseBadguyWidget.cs
+++ supertux-editor/PropertyEditors/ChooseBadguyWidget.cs
@@ -343,71 +343,16 @@ public class BadguyChooserWidget : GLWidgetBase
 /// </summary>
 public sealed class ChooseBadguyWidget : CustomSettingsWidget
 {
-	private Entry entry;
-	private bool changed = false;
-
 	public override Widget Create(object caller)
 	{
-		VBox vBox = new VBox(false, 3);
-
-		List<string> val = (List<string>) field.GetValue(Object);
-
-		entry = new Entry();
-		BadguyChooserWidget editor = new BadguyChooserWidget(val);
-
-		string String = "";
-		for (int i = 0; i < val.Count; i++) {
-			String += val[i];
-			if (i < val.Count -1) 
-				String += ", ";
-		}
-		entry.Text = String; 
-
-		entry.Changed += OnBadguyChanged;
-		entry.FocusOutEvent += OnBadguyChangeDone;
-
-		vBox.PackEnd(entry, true, true, 0);
-		vBox.PackEnd(editor, true, true, 0);
+		BadguyChooserWidget editor = new BadguyChooserWidget((List<string>)field.GetValue(Object));
 
 		// Create a tooltip if we can.
-		CreateToolTip(caller, vBox);
+		CreateToolTip(caller, editor);
 
-		vBox.Name = field.Name;
+		editor.Name = field.Name;
 
-		return vBox;
-	}
-
-	private void OnBadguyChanged(object sender, EventArgs args)
-	{
-		changed = true;
-	}
-
-	private void OnBadguyChangeDone(object sender, EventArgs args)
-	{
-		if (!changed) return; 
-		changed = false;
-		List<string> bad = new List<string>();
-		string String1 = entry.Text;
-		string String2 = "";
-
-		while (String1.IndexOf(", ")>-1) {	//at least two not yet added items
-			String2 = String1.Substring(0, String1.IndexOf(", "));
-			String1 = String1.Substring(String1.IndexOf(", ")+2, String1.Length-String1.IndexOf(", ")-2);
-			bad.Add(String2);
-		}
-
-		if (String1.Length>0){
-			bad.Add(String1);
-
-		}
-
-		PropertyChangeCommand command = new PropertyChangeCommand(
-			"Changed " + field.Name,
-			field,
-			Object,
-			bad);
-		command.Do();
-		UndoManager.AddCommand(command);
+		return editor;
 	}
 }
 
