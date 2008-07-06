bed2060b09f681886148bdae04043f5b2efff476
Miloš Klouček <m.kloucek.m@atlas.cz>
2008-07-06 09:48:47 +0000

    EDITOR: Draw tilemap itself where it should be (according to X and Y value)
    SVN-Revision: 5667

--- supertux-editor/Tiles/TilemapNode.cs
+++ supertux-editor/Tiles/TilemapNode.cs
@@ -7,30 +7,34 @@ using System;
 ///	A SceneGraph Node which draws a tilemap
 /// </summary>
 public sealed class TilemapNode : Node {
-	private Field<int> Field;
+	private Tilemap tilemap;
 	private Tileset Tileset;
 
-	public TilemapNode(Field<int> Field, Tileset Tileset) {
-		this.Field = Field;
+	public TilemapNode(Tilemap tilemap, Tileset Tileset) {
+		this.tilemap = tilemap;
 		this.Tileset = Tileset;
 	}
 
 	public void Draw(Gdk.Rectangle cliprect) {
+		cliprect.X -= (int)tilemap.X;
+		cliprect.Y -= (int)tilemap.Y;
+
 		uint start_x = (uint) Math.Max(0, cliprect.X / 32 - 1);
 		uint start_y = (uint) Math.Max(0, cliprect.Y / 32 - 1);
 		//HACK: Couldn't find any "inrange" or such
-		uint end_x = (uint) Math.Max(0, Math.Min(Field.Width, (cliprect.X + cliprect.Width) / 32 + 1));
-		uint end_y = (uint) Math.Max(0, Math.Min(Field.Height, (cliprect.Y + cliprect.Height) / 32 + 1));
+		uint end_x = (uint) Math.Max(0, Math.Min(tilemap.Width, (cliprect.X + cliprect.Width) / 32 + 1));
+		uint end_y = (uint) Math.Max(0, Math.Min(tilemap.Height, (cliprect.Y + cliprect.Height) / 32 + 1));
 		Tile Tile;
 		for (uint y = start_y; y < end_y; ++y) {
 			for (uint x = start_x; x < end_x; ++x) {
-				Tile = Tileset.Get(Field[x, y]);
+				Tile = Tileset.Get(tilemap[x, y]);
 				if (Tile != null)
-					Tile.DrawEditor(new Vector(x * 32, y * 32));
+					Tile.DrawEditor(new Vector(x * 32 + tilemap.X,
+									y * 32 + tilemap.Y));
 				else
 					LogManager.Log(LogLevel.Warning,
 					               "Tile {0} is null?! The tile with id {0} at {1},{2} is probably invalid.",
-					               Field[x, y], x, y);
+					               tilemap[x, y], x, y);
 			}
 		}
 	}
