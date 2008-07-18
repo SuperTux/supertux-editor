e75b83bf0685bde0a7921df925f84fda32237d03
Miloš Klouček <m.kloucek.m@atlas.cz>
2008-07-18 17:43:34 +0000

    Added special GraphScene.Node for drawing border around active tilemap.
    SVN-Revision: 5676

---
+++ supertux-editor/Tiles/TilemapBorder.cs
@@ -0,0 +1,42 @@
+//  $Id$
+using SceneGraph;
+using OpenGl;
+
+/// <summary>
+///	A SceneGraph Node which draws border around currently active tilemap in application.
+/// </summary>
+public sealed class TilemapBorder : Node {
+
+	public IEditorApplication application;
+
+	public TilemapBorder(IEditorApplication application)
+	{
+		this.application = application;
+	}
+
+	public void Draw(Gdk.Rectangle cliprect)
+	{
+		if (application.CurrentTilemap == null)
+			return;		//Stop, if we have no tilemap
+		if (application.CurrentTilemap.Width ==  application.CurrentSector.Width &&
+			application.CurrentTilemap.Width ==  application.CurrentSector.Width &&	
+			application.CurrentTilemap.X == 0 &&
+			application.CurrentTilemap.Y == 0)
+			return;		//Stop, if we have full-sized tilemap starting at [0,0]
+
+		Tilemap tm = application.CurrentTilemap;
+
+		gl.Disable(gl.TEXTURE_2D);
+		gl.PolygonMode(gl.FRONT_AND_BACK, gl.LINE);
+		gl.Begin(gl.QUADS);
+
+		gl.Vertex2f(tm.X - 1					, tm.Y - 1);
+		gl.Vertex2f(tm.X + 1 + tm.Width * Tileset.TILE_WIDTH	, tm.Y - 1);
+		gl.Vertex2f(tm.X + 1 + tm.Width * Tileset.TILE_WIDTH	, tm.Y + 1 + tm.Height * Tileset.TILE_HEIGHT);
+		gl.Vertex2f(tm.X - 1					, tm.Y + 1 + tm.Height * Tileset.TILE_HEIGHT);
+		gl.End();
+
+		gl.PolygonMode(gl.FRONT_AND_BACK, gl.FILL);
+		gl.Enable(gl.TEXTURE_2D);
+	}
+}
