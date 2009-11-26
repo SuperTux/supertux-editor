cb1dd5bf1c69eed579121d73cd5a0463c1b51aa0
Ingo Ruhnke <grumbel@gmx.de>
2009-11-26 02:17:51 +0000

    Added proper GPLv3+ header
    SVN-Revision: 6090

--- supertux-editor/AssemblyInfo.cs
+++ supertux-editor/AssemblyInfo.cs
@@ -1,4 +1,19 @@
-//  $Id$
+//  SuperTux Editor
+//  Copyright (C) 2006 Matthias Braun <matze@braunis.de>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
@@ -11,3 +26,5 @@ using System.Security.Permissions;
 [assembly: CLSCompliant(false)]
 [assembly: SecurityPermission(SecurityAction.RequestMinimum,
                               Execution = true, UnmanagedCode = true, Unrestricted = true)]
+
+/* EOF */
--- supertux-editor/Constants.cs
+++ supertux-editor/Constants.cs
@@ -1,5 +1,22 @@
-//  $Id$
+//  SuperTux
+//  Copyright (C) 2006 Matthias Braun <matze@braunis.de>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 public static class Constants {
 	public const string PACKAGE_NAME = "supertux-editor";
 	public const string PACKAGE_VERSION = "0.3-svn";
 }
+
+/* EOF */
