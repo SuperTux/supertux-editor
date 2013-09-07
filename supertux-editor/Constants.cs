b652f955b67181956cee7765599df8d7ec62bb5d
LMH <lmh.0013@gmail.com>
2013-09-07 08:32:45 -1000

    Changed the editor version to 0.3.4-GIT

--- supertux-editor/Constants.cs
+++ supertux-editor/Constants.cs
@@ -16,7 +16,7 @@
 
 public static class Constants {
 	public const string PACKAGE_NAME = "supertux-editor";
-	public const string PACKAGE_VERSION = "0.3-svn";
+	public const string PACKAGE_VERSION = "0.3.4-GIT";
 }
 
 /* EOF */
